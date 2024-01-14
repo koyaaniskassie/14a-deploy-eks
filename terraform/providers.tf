fb562a7743002126834d44df65512a410d56228b
rafnow1403 <136935131+rafnow1403@users.noreply.github.com>
2024-01-14 22:54:31 +0100

    selfmanaged argocd

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,19 +1,19 @@
-data "aws_eks_cluster" "cluster" {
-  name  = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name  = module.eks.cluster_name
+# }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-  config_context =  data.aws_eks_cluster.cluster.arn 
-}
+# provider "kubernetes" {
+#   config_path = "~/.kube/config"
+#   config_context =  data.aws_eks_cluster.cluster.arn 
+# }
 
 provider "aws" {
   region  = var.aws_region
   profile = terraform.workspace
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
