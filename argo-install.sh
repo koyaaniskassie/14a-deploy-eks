4ac3ab47cec37e04e91d815957dca101bcc273a5
rafnow1403 <136935131+rafnow1403@users.noreply.github.com>
2024-01-21 21:16:33 +0100

    created shell to speed up initialization process

---
+++ argo-install.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+cd k8s/argocd
+helm repo add argo-helm https://argoproj.github.io/argo-helm
+helm dependency build
+helm install argocd . -f values.yaml -f values-secrets.yaml --namespace argocd --create-namespace
+cd ../argocd-apps
+helm dependency build
+helm install argocd-apps . -f values.yaml --namespace argocd --create-namespace
