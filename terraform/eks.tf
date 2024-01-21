973e9466f51bc124b87b0e74ff5773ee26f49e8d
rafnow1403 <136935131+rafnow1403@users.noreply.github.com>
2024-01-21 22:10:05 +0100

    upgrade to 3 nodes

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -73,8 +73,8 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      max_size     = 3
+      desired_size = 2
     }
 
     two = {
