77b4383fc614f1df197bffae7593ccfb12ec04e7
rafnow1403 <136935131+rafnow1403@users.noreply.github.com>
2024-01-22 23:20:15 +0100

    updated init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 cd terraform
 terraform workspace select brasov
 terraform apply -auto-approve
+aws eks --region eu-central-1 update-kubeconfig --name education-eks --profile brasov
 cd ../k8s/argocd
 helm repo add argo-helm https://argoproj.github.io/argo-helm
 helm dependency build
